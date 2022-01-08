9c81e14555e7923173e586a7a3d332f93ca26d1e
brahim kehel <brahimkehel@gmail.com>
2022-01-08 20:02:31 +0100

    initializing spring boot project

---
+++ src/main/java/com/emsi/gowithus/GowithusApplication.java
@@ -0,0 +1,13 @@
+package com.emsi.gowithus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GowithusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GowithusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emsi/gowithus/GowithusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emsi.gowithus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GowithusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
